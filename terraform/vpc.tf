271395114d4d5d659e7c8008e12d050845aacb55
Jonas Bechstein <jonas.bechstein@cosee.biz>
2020-01-28 23:45:21 +0100

    [tf] fix subnet creation

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "demo" {
   count = 3
 
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = "10.0.${count.index + count.index * 32}.0/19"
+  cidr_block        = "10.0.${count.index}.0/24"
   vpc_id            = aws_vpc.demo.id
 
   tags = map(
